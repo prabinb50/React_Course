40beb52e257fbdea2f180abf0e93ed2a77e0fb9a
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-03 19:20:05 +0545

    update

--- 18_Fragments/Learning_Fragments/src/App.jsx
+++ 18_Fragments/Learning_Fragments/src/App.jsx
@@ -2,16 +2,36 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 // import React from 'react'
 
+// Fragments
+// function App() {
+//   return (
+//     <>
+//       <h1>Healthy Foods</h1>
+//       <ol class="list-group list-group-numbered">
+//         <li class="list-group-item">Salad</li>
+//         <li class="list-group-item">Water</li>
+//         <li class="list-group-item">Milk</li>
+//       </ol>
+//     </>
+//   );
+// }
+
+// Map Methods
+// Rendering lists using Map Method
 function App() {
-  return <>
-    <h1>Healthy Foods</h1>
-    <ol class="list-group list-group-numbered">
-      <li class="list-group-item">Green Vegetables</li>
-      <li class="list-group-item">Milk</li>
-      <li class="list-group-item">Salad</li>
-      <li class="list-group-item">Whole Grains</li>
-    </ol>
-  </>
+
+  let foodItems = ["Salad", "Green Vegatables", "Milk", "Whole Grains", "Meat", "Ghee"];
+  return (
+    <>
+      <h1>Healthy Foods</h1>
+      <ol className="list-group list-group-numbered">
+        {foodItems.map((item) => (<li key={item}
+          className="list-group-item">{item}</li>))}
+      </ol>
+    </>
+  );
 }
+
+// Conditional Rendering
 
 export default App
