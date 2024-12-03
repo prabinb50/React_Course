23912b7052226f59deac33b47e969c5e8f6eb04d
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-03 18:55:05 +0545

    update

--- 18_Fragments/Learning_Fragments/src/App.jsx
+++ 18_Fragments/Learning_Fragments/src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+// import React from 'react'
 
 function App() {
-  return <div>Fragments</div>
+  return <>
+    <h1>Healthy Foods</h1>
+    <ol class="list-group list-group-numbered">
+      <li class="list-group-item">Green Vegetables</li>
+      <li class="list-group-item">Milk</li>
+      <li class="list-group-item">Salad</li>
+      <li class="list-group-item">Whole Grains</li>
+    </ol>
+  </>
 }
 
 export default App
