08e127310ffea57ebd5d418bcb2ac65cfddc0b30
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-16 11:22:24 +0545

    managing state

--- 25_Passing_Function_Via_Props/Learning/src/components/Props/FoodItems.jsx
+++ 25_Passing_Function_Via_Props/Learning/src/components/Props/FoodItems.jsx
@@ -1,19 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import Item from "./Item";
+import { useState } from "react";
 
 function FoodItems({ items }) {
+
+    let [activeItems, setActiveItems] = useState([]);
+
+    let onBuyButton = (item, event) => {
+        let newItems = [...activeItems, item];
+        setActiveItems(newItems);
+    }
 
     // let foodItems = ["Salad", "Green Vegatables", "Milk", "Whole Grains", "Meat", "Ghee"];
 
     return (
         <>
             <ol className="list-group list-group-numbered">
-                {items.map((item) => (<Item key={item} foodItems={item} handleBuyButton={() => {
-                    alert(`${item} being bought`);
-                }}></Item>))}
+                {items.map((item) => (<Item key={item} foodItems={item} bought={activeItems.includes(item)} handleBuyButton={(event) => onBuyButton(item, event)}></Item>))}
             </ol>
         </>
     )
 }
 
 export default FoodItems;
+
--- 25_Passing_Function_Via_Props/Learning/src/components/Props/Item.jsx
+++ 25_Passing_Function_Via_Props/Learning/src/components/Props/Item.jsx
@@ -1,10 +1,10 @@
 import styles from "./Item.module.css";
 
-function Item({ foodItems, handleBuyButton }) {
+function Item({ foodItems, bought, handleBuyButton }) {
 
     return (
         <li key={foodItems}
-            className={`${styles["kg-item"]} list-group-item`}>{foodItems}
+            className={`${styles["kg-item"]} list-group-item ${bought && 'active'}`}>{foodItems}
 
             <button className={`${styles.button} btn btn-outline-dark`} onClick={handleBuyButton}>Buy</button>
         </li>
@@ -12,3 +12,4 @@ function Item({ foodItems, handleBuyButton }) {
 }
 
 export default Item;
+
