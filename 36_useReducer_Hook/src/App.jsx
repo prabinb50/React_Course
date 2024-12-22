c7d5671c18db05b85b6054a6889b672b4e053a86
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-22 14:03:20 +0545

    useReducer Hook in todo app

--- 36_useReducer_Hook/src/App.jsx
+++ 36_useReducer_Hook/src/App.jsx
@@ -3,57 +3,18 @@ import AddTodo from "./Components/AddTodo"
 import "./style.css"
 import TodoItems from "./Components/TodoItems"
 import WelcomeMess from "./Components/WelcomeMess"
-import { useReducer } from "react";
-import { TodoItemsContext } from "./store/TodoItemsStore";
-
-// reducer is a pure function.... action object takes and return current state
-const todoItemsReducer = (currTodoItems, action) => {
-  let newTodoItems = currTodoItems;
-  if (action.type === "NEW_ITEM") {
-    newTodoItems = [...currTodoItems, { name: action.payload.itemName, date: action.payload.itemDate }];
-  } else if (action.type === "DELETE_ITEM") {
-    newTodoItems = currTodoItems.filter(item => item.name !== action.payload.itemName);
-  }
-  return newTodoItems;
-}
+import TodoItemsContextProvider from "./store/TodoItemsStore";
 
 function App() {
-
-  // const [todoItems, setTodoItems] = useState([]);
-
-  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
-
-  const addNewItem = (itemName, itemDate) => {
-    const newItemAction = {
-      type: "NEW_ITEM",
-      payload: {
-        itemName: itemName,
-        itemDate: itemDate
-      }
-    }
-    dispatchTodoItems(newItemAction);
-  }
-
-  const deleteItem = (todoItemName) => {
-    const deleteItemAction = {
-      type: "DELETE_ITEM",
-      payload: {
-        itemName: todoItemName,
-      }
-    }
-    dispatchTodoItems(deleteItemAction);
-  }
-
   return (
-    <TodoItemsContext.Provider value={{ todoItems: todoItems, addNewItem: addNewItem, deleteItem: deleteItem }}>
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName></AppName>
         <AddTodo></AddTodo>
         <WelcomeMess></WelcomeMess>
         <TodoItems></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
-
+    </TodoItemsContextProvider>
   )
 }
 export default App
--- 36_useReducer_Hook/src/store/TodoItemsStore.jsx
+++ 36_useReducer_Hook/src/store/TodoItemsStore.jsx
@@ -1,3 +1,46 @@
 import { createContext } from "react"
+import { useReducer } from "react";
 
 export const TodoItemsContext = createContext([]);
+
+// reducer is a pure function.... action object takes and return current state
+const todoItemsReducer = (currTodoItems, action) => {
+    let newTodoItems = currTodoItems;
+    if (action.type === "NEW_ITEM") {
+        newTodoItems = [...currTodoItems, { name: action.payload.itemName, date: action.payload.itemDate }];
+    } else if (action.type === "DELETE_ITEM") {
+        newTodoItems = currTodoItems.filter(item => item.name !== action.payload.itemName);
+    }
+    return newTodoItems;
+}
+
+const TodoItemsContextProvider = ({ children }) => {
+
+    const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+    const addNewItem = (itemName, itemDate) => {
+        const newItemAction = {
+            type: "NEW_ITEM",
+            payload: {
+                itemName: itemName,
+                itemDate: itemDate
+            }
+        }
+        dispatchTodoItems(newItemAction);
+    }
+
+    const deleteItem = (todoItemName) => {
+        const deleteItemAction = {
+            type: "DELETE_ITEM",
+            payload: {
+                itemName: todoItemName,
+            }
+        }
+        dispatchTodoItems(deleteItemAction);
+    }
+    return (
+        <TodoItemsContext.Provider value={{ todoItems: todoItems, addNewItem: addNewItem, deleteItem: deleteItem }}>{children}</TodoItemsContext.Provider>
+    )
+}
+
+export default TodoItemsContextProvider;
