99fa9ed8c560e4539f76b333723b88becb15718e
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-22 13:40:56 +0545

    Fix: Update fragments and remove unused Todo_App index.css

--- 18_Fragments/Learning_Fragments/src/App.jsx
+++ 18_Fragments/Learning_Fragments/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import ConditionalRendering from './ConditionalRendering';
 import Fragments from './Fragments';
@@ -6,17 +5,20 @@ import MapMethods from './MapMethods';
 import FoodItems from './components/Props/FoodItems';
 import ErrMessage from './components/Props/ErrMessage';
 import Heading from './components/Props/Heading';
+import "./App.css"
+
 
 // Fragments
 function App() {
+  let foodItems = ["Salad", "Green Vegatables", "Milk", "Whole Grains", "Meat", "Ghee"];
   return (
     <>
       {/* <Fragments></Fragments>
       <MapMethods></MapMethods>
       <ConditionalRendering /> */}
       <Heading></Heading>
-      <ErrMessage></ErrMessage>
-      <FoodItems></FoodItems>
+      <ErrMessage items={foodItems}></ErrMessage>
+      <FoodItems items={foodItems}></FoodItems>
     </>
   );
 }
