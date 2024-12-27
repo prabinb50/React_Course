067efb10433afb5346bb38942b589b416d934353
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-27 14:04:42 +0545

    simple social media project

---
+++ Simple_Projects/Social_Media/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import SideBar from './components/SideBar'
+
+function App() {
+  return (
+    <>
+      <SideBar></SideBar>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
