f95d0f8c7fa160c4a1d5ab425fcf3d6584595486
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-27 15:09:52 +0545

    simple social media project

--- Simple_Projects/Social_Media/src/App.jsx
+++ Simple_Projects/Social_Media/src/App.jsx
@@ -5,15 +5,18 @@ import Footer from './components/Footer'
 import SideBar from './components/SideBar'
 import CreatePost from './components/CreatePost'
 import PostList from './components/PostList'
+import { useState } from 'react'
 
 function App() {
+
+  const [selectedTab, setSelectedTab] = useState("Home");
+
   return (
     <div className='app-container'>
-      <SideBar></SideBar>
+      <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab}></SideBar>
       <div className='content'>
         <Header></Header>
-        <CreatePost></CreatePost>
-        <PostList></PostList>
+        {selectedTab === 'Home' ? (<PostList></PostList>) : (<CreatePost></CreatePost>)}
         <Footer></Footer>
       </div>
     </div>
--- Simple_Projects/Social_Media/src/components/CreatePost.jsx
+++ Simple_Projects/Social_Media/src/components/CreatePost.jsx
@@ -2,12 +2,12 @@ const CreatePost = () => {
     return (
         <form>
             <div className="mb-3">
-                <label for="exampleInputEmail1" className="form-label">Email address</label>
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
                 <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
-                <label for="exampleInputPassword1" className="form-label">Password</label>
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                 <input type="password" className="form-control" id="exampleInputPassword1" />
             </div>
             <div className="mb-3 form-check">
