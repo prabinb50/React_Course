7455f77c7ccdaadd01e2bb826445e184c46c69b7
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-30 11:52:54 +0545

    simple social media project update

--- Simple_Projects/Social_Media/src/store/post-list-store.jsx
+++ Simple_Projects/Social_Media/src/store/post-list-store.jsx
@@ -12,6 +12,8 @@ const postListReducer = (currentPostList, action) => {
 
     if (action.type === 'DELETE_POST') {
         newPostList = currentPostList.filter(post => post.id !== action.payload.postId)
+    } else if (action.type === 'ADD_POST') {
+        newPostList = [action.payload, ...currentPostList];
     }
     return newPostList;
 }
@@ -21,6 +23,17 @@ const PostListProvider = ({ children }) => {
     const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
 
     const addPost = (userId, postTitle, postBody, reactions, tags) => {
+        dispatchPostList({
+            type: 'ADD_POST',
+            payload: {
+                id: Date.now(),
+                title: postTitle,
+                body: postBody,
+                reaction: reactions,
+                userId: userId,
+                tags: tags
+            }
+        })
 
     }
 
