5be9e1788d7066bacc50a7c144432881210ee95f
Prabin Joshi <joshiprabin17@gmail.com>
2024-12-31 10:30:57 +0545

    handling loading state in social media project

---
+++ 40_Handling_Loading_State/src/components/PostList.jsx
@@ -0,0 +1,32 @@
+import { useContext, useEffect, useState } from "react"
+import Post from "./Post"
+import { PostList as PostListData } from "../store/post-list-store"
+import WelcomeMess from "./WelcomeMess";
+import LoadingSpinner from "./LoadingSpinner";
+
+const PostList = () => {
+
+    const { postList, addInitialPosts } = useContext(PostListData);
+
+    const [fetching, setFetching] = useState(false);
+
+    useEffect(() => {
+        setFetching(true);
+        fetch('https://dummyjson.com/posts')
+            .then(res => res.json())
+            .then(data => {
+                addInitialPosts(data.posts);
+                setFetching(false);
+            });
+    }, []);
+
+    return (
+        <>
+            {fetching && <LoadingSpinner></LoadingSpinner>}
+            {!fetching && postList.length === 0 && <WelcomeMess></WelcomeMess>}
+            {!fetching && postList.map((post) => <Post key={post.id} post={post}></Post>)}
+        </>
+    )
+}
+
+export default PostList
