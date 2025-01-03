88b79cd171133782ee64d7cd31a9df55d3f1bda3
Prabin Joshi <joshiprabin17@gmail.com>
2025-01-03 20:59:34 +0545

    learn Layout Routes & Routes Links in social media project

--- 47_React_Router/src/main.jsx
+++ 47_React_Router/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './routes/App.jsx'
+import App from './routes/App.jsx';
 import CreatePost from './components/CreatePost.jsx';
 import PostList from './components/PostList.jsx';
 
