8b7d2335a12c3a0558e5735825f090a913559e48
Prabin Joshi <joshiprabin17@gmail.com>
2025-01-18 21:46:21 +0545

    updated code of myntra clone

--- Simple_Projects/Myntra_Clone/3_Myntra_React_Clone/src/main.jsx
+++ Simple_Projects/Myntra_Clone/3_Myntra_React_Clone/src/main.jsx
@@ -5,6 +5,8 @@ import App from './routes/App.jsx'
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Bag from './routes/Bag.jsx'
 import Home from './routes/Home.jsx'
+import { Provider } from "react-redux"
+import myntraStore from './store/index.js'
 
 const router = createBrowserRouter([
   {
@@ -25,6 +27,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <Provider store={myntraStore}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </StrictMode>,
 )
--- Simple_Projects/Myntra_Clone/3_Myntra_React_Clone/src/routes/App.jsx
+++ Simple_Projects/Myntra_Clone/3_Myntra_React_Clone/src/routes/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import FetchItem from "../components/FetchItem"
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <FetchItem></FetchItem>
       <Outlet></Outlet>
       <Footer></Footer>
     </>
